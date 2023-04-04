6d5b1b76fcb9c42f91f9c6f3a266313c14b4ea95
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-04 15:36:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,79 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+import gspread
+import requests
+from flask import Flask
+from oauth2client.service_account import ServiceAccountCredentials
+from tchan import ChannelScraper
 
 @app.route("/")
 def hello_world():
    return "Bem vindo! Este site contém informações sobre o Campeonato Brasileiro"
+
+menu = """
+<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
+
+@app.route("/")
+def index():
+  return menu + "Olá, mundo! Esse é meu site. (Sérgio Vieira)"
+
+@app.route("/sobre")
+def sobre():
+  return menu + "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+  return menu + "Aqui vai o conteúdo da página Contato"
+
+
+@app.route("/promocoes")
+def promocoes():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  for promocao in ultimas_promocoes():
+    conteudo += f"<li>{promocao}</li>"
+  return conteudo + "</ul>"
+
+
+@app.route("/promocoes2")
+def promocoes2():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
+  return conteudo + "</ul>"
+
+@app.route("/dedoduro")
+def dedoduro():
+  mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
+  resposta = requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
+  return f"Mensagem enviada. Resposta ({resposta.status_code}): {resposta.text}"
+
+
+@app.route("/dedoduro2")
+def dedoduro2():
+  sheet.append_row(["Sérgio", "Vieira", "a partir do Flask"])
+  return "Planilha escrita!"
+
+@app.route("/telegram-bot", methods=["POST"])
+def telegram_bot():
+  update = request.json
+  chat_id = update["message"]["chat"]["id"]
+  message = update["message"]["text"]
+  nova_mensagem = {"chat_id": chat_id, "text": message}
+  requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+  return "ok"
