9b8163aab3d71afbbd0c65b871502e994417eafd
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-04 20:25:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 import gspread
 import requests
 from flask import Flask
@@ -47,27 +46,25 @@ def campeonatobrasileiro_bot():
     message = update["message"]["text"]
     nova_mensagem = {"chat_id": chat_id, "text": message}
     requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  
-    if message == "/start":  
-        texto_resposta = "Olá! Seja bem-vinda(o). Você quer saber sobre qual time do Campeonato Brasileiro?"
+ 
     
-    elif message in ['Palmeiras', 'Flamengo', 'Corinthians', 'Sao Paulo', 'Atletico Mineiro', 'Internacional', 'Ceara', 'Bahia', 'Athletico Paranaense', 'Chapecoense', 'Cuiaba', 'Fluminense', 'Palmeiras', 'Santos', 'America-MG', 'Gremio', 'Fortaleza', 'Sport', 'Red Bull Bragantino', 'Juventude', 'Atletico-GO']:
-        texto_resposta = "Olá! Aqui estão os resultados do {message} na temporada"
-        
-       for message in df:
-        df = pd.read_excel('https://github.com/SerginhoVN/Trabalho-Final-Campeonato-Brasileiro/blob/main/Jogos_Temporada_2021_SerieAB.xlsx')
+    if message == "/start":
+        texto_resposta = "Olá! Seja bem-vindo(a). Qual time você gostaria de saber os resultados na temporada?"
+    
+    elif message in ['Palmeiras', 'Flamengo', 'Corinthians', 'Sao Paulo', 'Atletico Mineiro', 'Internacional', 'Ceara', 'Bahia', 'Athletico Paranaense', 'Chapecoense', 'Cuiaba', 'Fluminense', 'Santos', 'America-MG', 'Gremio', 'Fortaleza', 'Sport', 'Red Bull Bragantino', 'Juventude', 'Atletico-GO']:
+        df = pd.read_excel('https://github.com/SerginhoVN/Trabalho-Final-Campeonato-Brasileiro/raw/main/Jogos_Temporada_2021_SerieAB.xlsx')
         dffiltrado = df[(df.mandante == message) | (df.visitante == message)]
-        texto_resposta = f'{texto_resposta} \n \n{message}'
-   
-     else:
-      texto_resposta = "Não entendi! Diga o nome do seu time sem acento e veja os resultados dele no Campeonato Carioca"
-         
-  return "ok"
-
+        texto_resposta = f"Aqui estão os resultados do {message} na temporada:\n{dffiltrado.to_string(index=False)}"
+    
+    else:
+        texto_resposta = "Não entendi! Diga /start para começar."
+    
+    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+    requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+    
+    return "ok"
 
       #incluir pipedream para deixar recorrente 
-
-
 
 
 
