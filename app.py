35d98b6314371c1be747c398d01e6f6188471811
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-05 00:02:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import gspread
 import requests
-from flask import Flask
+from flask import Flask, request
 from oauth2client.service_account import ServiceAccountCredentials
 from tchan import ChannelScraper
 import pandas as pd
