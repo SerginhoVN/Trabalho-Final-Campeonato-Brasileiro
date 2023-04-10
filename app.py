b6b892310f6afe1edae725a613a1fcc92f11c6e4
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-10 13:19:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -110,5 +110,4 @@ def campeonatobrasileiro_bot():
       
       nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
       requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem,)
-    
-return "ok"
+      return "ok"
