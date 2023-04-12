4130f6fd1f517b299ccea55bff10fffb85a9972f
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-12 20:13:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def campeonatobrasileiro_bot():
   elif "@" in message:
       texto_resposta = "Obrigado! Vamos te cadastrar para receber as informações solicitadas"
       email = Mail(
-      from_email='sergio.vieira@inpresspni.com.br',
+      from_email='serginho.vieira.rio@gmail.com',
       to_emails=message,
       subject='Campeonato Brasileiro',
       html_content = texto_resposta_time
