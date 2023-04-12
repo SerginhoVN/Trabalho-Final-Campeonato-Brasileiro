3273a8c7984831f6dbaa1e736a8a8f478e46d039
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-12 18:48:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -126,21 +126,21 @@ elif "@" in message:
     sg = SendGridAPIClient(SENDGRID_KEY)
     response = sg.send(email)
    
-  else:
+ else:
     texto_resposta = "Não entendi! Diga 'oi' para começar."
 
-  if texto_resposta_time:
+ if texto_resposta_time:
     texto_resposta = texto_resposta_time 
   
-  nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
-  requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  mensagens.append([datahora, "enviada", first_name, chat_id, texto_resposta])
-  CampeonatoBrasileiro.append_row([datahora, first_name, chat_id, message])  
-  return "ok"
+ nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+ requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+ mensagens.append([datahora, "enviada", first_name, chat_id, texto_resposta])
+ CampeonatoBrasileiro.append_row([datahora, first_name, chat_id, message])  
+ return "ok"
 
-#Sendgrid
-@app.route("/send-email")
-def send_email():
-  emails=[]
-  respostas = sheet.col_values(4)
-  return "ok"
+ #Sendgrid
+ @app.route("/send-email")
+ def send_email():
+   emails=[]
+   respostas = sheet.col_values(4)
+   return "ok"
