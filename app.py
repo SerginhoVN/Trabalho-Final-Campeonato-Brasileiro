73dce013a16487d5ed7628f825d5acb9d05cce58
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-13 14:51:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -134,8 +134,7 @@ def campeonatobrasileiro_bot():
   
   nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
   requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  mensagens.append([datahora, "enviada", first_name, chat_id, texto_resposta])
-  sheet.append_row([datahora, first_name, chat_id, message])  
+
   return "ok"
 
 #Sendgrid
