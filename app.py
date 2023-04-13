d69ec485cfd97484bda8dddd706e20c04ee23220
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-04-13 19:22:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def campeonatobrasileiro_bot():
       from_email='serginhosp21@gmail.com',
       to_emails=message,
       subject='Campeonato Brasileiro',
-      html_content = f"Confira as últimas rodadas do {message in times}":
+      html_content = f"Confira as últimas rodadas do {message in times}"
       )
       sg = SendGridAPIClient(SENDGRID_KEY)
       response = sg.send(email)
